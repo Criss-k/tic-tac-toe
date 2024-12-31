710c6d4e407f35489a0071c4af1971490159c29f
Criss K <38414961-veoki83@users.noreply.replit.com>
2024-12-31 09:29:30 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main className='text-3xl font-bold underline text-center my-8'>
+      React ⚛️ + Vite ⚡ + Replit 🌀
+    </main>
+  )
+}
